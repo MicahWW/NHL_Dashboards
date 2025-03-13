51a38edec9bfbdcf82f12c1e3127c6267f099bc1
Micah_Warren <26724012+MicahWW@users.noreply.github.com>
2025-03-12 22:26:16 -0500

    text sizing and positioning

--- wwwroot/js/resizeText.js
+++ wwwroot/js/resizeText.js
@@ -3,6 +3,22 @@ function resizeText() {
 
     for (let i=0; i<resizeElements.length; i++) {
         const maxHeight = resizeElements[i].clientHeight;
+        const maxWidth = resizeElements[i].clientWidth;
+        let fontSize = 1;
+        resizeElements[i].style.fontSize = fontSize + "px";
+
+        while (resizeElements[i].scrollHeight <= maxHeight && resizeElements[i].scrollWidth <= maxWidth) {
+            resizeElements[i].style.fontSize = ++fontSize + "px";
+        }
+        resizeElements[i].style.fontSize = --fontSize + "px";
+    }
+}
+
+function resizeTextToFitHeight() {
+    const resizeElements = document.getElementsByClassName("resize-text-height");
+
+    for (let i=0; i<resizeElements.length; i++) {
+        const maxHeight = resizeElements[i].clientHeight;
         let fontSize = 1;
         resizeElements[i].style.fontSize = fontSize + "px";
 
