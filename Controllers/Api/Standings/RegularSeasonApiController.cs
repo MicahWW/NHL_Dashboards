cd9397bfbda9232d2549d7f74d584c135031fa08
Micah_Warren <26724012+MicahWW@users.noreply.github.com>
2025-03-24 00:19:57 -0500

    swagger documentation

--- Controllers/Api/Standings/RegularSeasonApiController.cs
+++ Controllers/Api/Standings/RegularSeasonApiController.cs
@@ -18,6 +18,15 @@ public class RegularSeasonApiController : ControllerBase
         _httpClientFactory = httpClientFactory;
     }
 
+
+    /// <summary>
+    /// Gets the Regular Season data from the NHL
+    /// </summary>
+    /// <param name="date">
+    /// If you want to get the standings at a particular date list it here in YYYY-MM-DD format. Leaving this blank
+    /// will get the latest date posted by the NHL.
+    /// </param>
+    /// <returns>The NHL teams arranged into the RegularSeasonStandingsApiModel</returns>
     [HttpGet]
     [OpenApiTag("Standings")]
     [ProducesResponseType<RegularSeasonStandingsApiModel>(StatusCodes.Status200OK)]
@@ -32,9 +41,9 @@ public class RegularSeasonApiController : ControllerBase
             Standings = await NhlApi.GetRegularSeasonStandings(httpClient, date);
         }
         catch (Exception ex)
-                    {
-                        return Problem
-                        (
+        {
+            return Problem
+            (
                 title: "Getting Standings data from NHL",
                 detail: ex.Message,
                 statusCode: 500
@@ -42,112 +51,112 @@ public class RegularSeasonApiController : ControllerBase
         }
         var output = new RegularSeasonStandingsApiModel();
 
-                    // iterate through standings and assign to the appropriate position
+        // iterate through standings and assign to the appropriate position
         foreach (var standing in Standings.Standings)
-                    {
+        {
             var teamData = new RegularSeasonStandingsApiModel.TeamData(standing);
 
-                        switch (standing)
-                        {
-                            case { ConferenceName: "Western", WildcardSequence: 1 }:
-                                output.WesternWildcard1 = teamData;
-                                break;
-                            case { ConferenceName: "Western", WildcardSequence: 2 }:
-                                output.WesternWildcard2 = teamData;
-                                break;
-                            case { ConferenceName: "Western", WildcardSequence: 3 }:
-                                output.WesternWildcard3 = teamData;
-                                break;
-                            case { ConferenceName: "Western", WildcardSequence: 4 }:
-                                output.WesternWildcard4 = teamData;
-                                break;
-                            case { ConferenceName: "Western", WildcardSequence: 5 }:
-                                output.WesternWildcard5 = teamData;
-                                break;
-                            case { ConferenceName: "Western", WildcardSequence: 6 }:
-                                output.WesternWildcard6 = teamData;
-                                break;
-                            case { ConferenceName: "Western", WildcardSequence: 7 }:
-                                output.WesternWildcard7 = teamData;
-                                break;
-                            case { ConferenceName: "Western", WildcardSequence: 8 }:
-                                output.WesternWildcard8 = teamData;
-                                break;
-                            case { ConferenceName: "Western", WildcardSequence: 9 }:
-                                output.WesternWildcard9 = teamData;
-                                break;
-                            case { ConferenceName: "Western", WildcardSequence: 10 }:
-                                output.WesternWildcard10 = teamData;
-                                break;
-                            case { ConferenceName: "Western", DivisionSequence: 1, DivisionName: "Central" }:
-                                output.Central1 = teamData;
-                                break;
-                            case { ConferenceName: "Western", DivisionSequence: 2, DivisionName: "Central" }:
-                                output.Central2 = teamData;
-                                break;
-                            case { ConferenceName: "Western", DivisionSequence: 3, DivisionName: "Central" }:
-                                output.Central3 = teamData;
-                                break;
-                            case { ConferenceName: "Western", DivisionSequence: 1, DivisionName: "Pacific" }:
-                                output.Pacific1 = teamData;
-                                break;
-                            case { ConferenceName: "Western", DivisionSequence: 2, DivisionName: "Pacific" }:
-                                output.Pacific2 = teamData;
-                                break;
-                            case { ConferenceName: "Western", DivisionSequence: 3, DivisionName: "Pacific" }:
-                                output.Pacific3 = teamData;
-                                break;
-                            case { ConferenceName: "Eastern", WildcardSequence: 1 }:
-                                output.EasternWildcard1 = teamData;
-                                break;
-                            case { ConferenceName: "Eastern", WildcardSequence: 2 }:
-                                output.EasternWildcard2 = teamData;
-                                break;
-                            case { ConferenceName: "Eastern", WildcardSequence: 3 }:
-                                output.EasternWildcard3 = teamData;
-                                break;
-                            case { ConferenceName: "Eastern", WildcardSequence: 4 }:
-                                output.EasternWildcard4 = teamData;
-                                break;
-                            case { ConferenceName: "Eastern", WildcardSequence: 5 }:
-                                output.EasternWildcard5 = teamData;
-                                break;
-                            case { ConferenceName: "Eastern", WildcardSequence: 6 }:
-                                output.EasternWildcard6 = teamData;
-                                break;
-                            case { ConferenceName: "Eastern", WildcardSequence: 7 }:
-                                output.EasternWildcard7 = teamData;
-                                break;
-                            case { ConferenceName: "Eastern", WildcardSequence: 8 }:
-                                output.EasternWildcard8 = teamData;
-                                break;
-                            case { ConferenceName: "Eastern", WildcardSequence: 9 }:
-                                output.EasternWildcard9 = teamData;
-                                break;
-                            case { ConferenceName: "Eastern", WildcardSequence: 10 }:
-                                output.EasternWildcard10 = teamData;
-                                break;
-                            case { ConferenceName: "Eastern", DivisionSequence: 1, DivisionName: "Metropolitan" }:
-                                output.Metropolitan1 = teamData;
-                                break;
-                            case { ConferenceName: "Eastern", DivisionSequence: 2, DivisionName: "Metropolitan" }:
-                                output.Metropolitan2 = teamData;
-                                break;
-                            case { ConferenceName: "Eastern", DivisionSequence: 3, DivisionName: "Metropolitan" }:
-                                output.Metropolitan3 = teamData;
-                                break;
-                            case { ConferenceName: "Eastern", DivisionSequence: 1, DivisionName: "Atlantic" }:
-                                output.Atlantic1 = teamData;
-                                break;
-                            case { ConferenceName: "Eastern", DivisionSequence: 2, DivisionName: "Atlantic" }:
-                                output.Atlantic2 = teamData;
-                                break;
-                            case { ConferenceName: "Eastern", DivisionSequence: 3, DivisionName: "Atlantic" }:
-                                output.Atlantic3 = teamData;
-                                break;
-                        }
-                    }
+            switch (standing)
+            {
+                case { ConferenceName: "Western", WildcardSequence: 1 }:
+                    output.WesternWildcard1 = teamData;
+                    break;
+                case { ConferenceName: "Western", WildcardSequence: 2 }:
+                    output.WesternWildcard2 = teamData;
+                    break;
+                case { ConferenceName: "Western", WildcardSequence: 3 }:
+                    output.WesternWildcard3 = teamData;
+                    break;
+                case { ConferenceName: "Western", WildcardSequence: 4 }:
+                    output.WesternWildcard4 = teamData;
+                    break;
+                case { ConferenceName: "Western", WildcardSequence: 5 }:
+                    output.WesternWildcard5 = teamData;
+                    break;
+                case { ConferenceName: "Western", WildcardSequence: 6 }:
+                    output.WesternWildcard6 = teamData;
+                    break;
+                case { ConferenceName: "Western", WildcardSequence: 7 }:
+                    output.WesternWildcard7 = teamData;
+                    break;
+                case { ConferenceName: "Western", WildcardSequence: 8 }:
+                    output.WesternWildcard8 = teamData;
+                    break;
+                case { ConferenceName: "Western", WildcardSequence: 9 }:
+                    output.WesternWildcard9 = teamData;
+                    break;
+                case { ConferenceName: "Western", WildcardSequence: 10 }:
+                    output.WesternWildcard10 = teamData;
+                    break;
+                case { ConferenceName: "Western", DivisionSequence: 1, DivisionName: "Central" }:
+                    output.Central1 = teamData;
+                    break;
+                case { ConferenceName: "Western", DivisionSequence: 2, DivisionName: "Central" }:
+                    output.Central2 = teamData;
+                    break;
+                case { ConferenceName: "Western", DivisionSequence: 3, DivisionName: "Central" }:
+                    output.Central3 = teamData;
+                    break;
+                case { ConferenceName: "Western", DivisionSequence: 1, DivisionName: "Pacific" }:
+                    output.Pacific1 = teamData;
+                    break;
+                case { ConferenceName: "Western", DivisionSequence: 2, DivisionName: "Pacific" }:
+                    output.Pacific2 = teamData;
+                    break;
+                case { ConferenceName: "Western", DivisionSequence: 3, DivisionName: "Pacific" }:
+                    output.Pacific3 = teamData;
+                    break;
+                case { ConferenceName: "Eastern", WildcardSequence: 1 }:
+                    output.EasternWildcard1 = teamData;
+                    break;
+                case { ConferenceName: "Eastern", WildcardSequence: 2 }:
+                    output.EasternWildcard2 = teamData;
+                    break;
+                case { ConferenceName: "Eastern", WildcardSequence: 3 }:
+                    output.EasternWildcard3 = teamData;
+                    break;
+                case { ConferenceName: "Eastern", WildcardSequence: 4 }:
+                    output.EasternWildcard4 = teamData;
+                    break;
+                case { ConferenceName: "Eastern", WildcardSequence: 5 }:
+                    output.EasternWildcard5 = teamData;
+                    break;
+                case { ConferenceName: "Eastern", WildcardSequence: 6 }:
+                    output.EasternWildcard6 = teamData;
+                    break;
+                case { ConferenceName: "Eastern", WildcardSequence: 7 }:
+                    output.EasternWildcard7 = teamData;
+                    break;
+                case { ConferenceName: "Eastern", WildcardSequence: 8 }:
+                    output.EasternWildcard8 = teamData;
+                    break;
+                case { ConferenceName: "Eastern", WildcardSequence: 9 }:
+                    output.EasternWildcard9 = teamData;
+                    break;
+                case { ConferenceName: "Eastern", WildcardSequence: 10 }:
+                    output.EasternWildcard10 = teamData;
+                    break;
+                case { ConferenceName: "Eastern", DivisionSequence: 1, DivisionName: "Metropolitan" }:
+                    output.Metropolitan1 = teamData;
+                    break;
+                case { ConferenceName: "Eastern", DivisionSequence: 2, DivisionName: "Metropolitan" }:
+                    output.Metropolitan2 = teamData;
+                    break;
+                case { ConferenceName: "Eastern", DivisionSequence: 3, DivisionName: "Metropolitan" }:
+                    output.Metropolitan3 = teamData;
+                    break;
+                case { ConferenceName: "Eastern", DivisionSequence: 1, DivisionName: "Atlantic" }:
+                    output.Atlantic1 = teamData;
+                    break;
+                case { ConferenceName: "Eastern", DivisionSequence: 2, DivisionName: "Atlantic" }:
+                    output.Atlantic2 = teamData;
+                    break;
+                case { ConferenceName: "Eastern", DivisionSequence: 3, DivisionName: "Atlantic" }:
+                    output.Atlantic3 = teamData;
+                    break;
+            }
+        }
 
-                    return Ok(output);
+        return Ok(output);
     }
 }
