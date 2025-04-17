5fa778e4f1e4881f509c328a2b35c75d41148c32
Micah_Warren <26724012+MicahWW@users.noreply.github.com>
2025-04-17 11:10:36 -0500

    use colors

--- wwwroot/js/playoffBrackets.js
+++ wwwroot/js/playoffBrackets.js
@@ -1,5 +1,6 @@
 // The URL to get the Standings data from
 let requestURL = "/Api/Standings/Playoffs";
+let colorsURL = "/Api/TeamDetails";
 
 // fetches data then passes it to the function to display it
 const getData = async () => {
@@ -10,11 +11,13 @@ const getData = async () => {
             displayData(jsonResponse);
         }
     } catch {
-        console.log(error);
+        console.log(`Get Data error\n ${error}`);
     }
 }
 
 function displayData(data) {
+    let colors = getColors();
+
     for (let i = 0; i < data.series.length; i++) {
         let ele = document.getElementById(`series-${data.series[i].seriesLetter.toLowerCase()}`)
 
@@ -22,11 +25,29 @@ function displayData(data) {
             let topTeam = ele.getElementsByClassName("top-team")[0];
             let botTeam = ele.getElementsByClassName("bot-team")[0];
 
+            let topTeamColors = colors[data.series[i].topTeam.abbr];
+            let botTeamColors = colors[data.series[i].botTeam.abbr];
+
+            topTeam.style.backgroundColor = `rgba(${topTeamColors.decimalRed}, ${topTeamColors.decimalGreen}, ${topTeamColors.decimalBlue}, 0.5)`;
+            botTeam.style.backgroundColor = `rgba(${botTeamColors.decimalRed}, ${botTeamColors.decimalGreen}, ${botTeamColors.decimalBlue}, 0.5)`;
+
             topTeam.getElementsByClassName("logo")[0].src = `https://assets.nhle.com/logos/nhl/svg/${data.series[i].topTeam.abbr}_dark.svg`;
             topTeam.getElementsByClassName("wins")[0].innerHTML = data.series[i].topTeam.wins;
             botTeam.getElementsByClassName("logo")[0].src = `https://assets.nhle.com/logos/nhl/svg/${data.series[i].botTeam.abbr}_dark.svg`;
             botTeam.getElementsByClassName("wins")[0].innerHTML = data.series[i].botTeam.wins;
         }
 
+    }
+}
+
+async function getColors() {
+    try {
+        const response = await fetch(colorsURL);
+        if (response.ok) {
+            const jsonResponse = await response.json();
+            return jsonResponse;
+        }
+    } catch {
+        console.log(`Get colors error\n${error}`);
     }
 }
