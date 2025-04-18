6d0d7867e2eb31e3f4efe9d8d49378fd95f8e057
Micah Warren <26724012+MicahWW@users.noreply.github.com>
2025-04-18 14:10:17 -0500

    display playoff data

--- wwwroot/js/playoffBrackets.js
+++ wwwroot/js/playoffBrackets.js
@@ -1,5 +1,9 @@
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+let year = urlParams.get("year");
+
 // The URL to get the Standings data from
-let requestURL = "/Api/Standings/Playoffs";
+let requestURL = year == null ? "/Api/Standings/Playoffs" : `/Api/Standings/Playoffs?year=${year}`;
 let colorsURL = "/Api/TeamDetails";
 
 // fetches data then passes it to the function to display it
@@ -30,10 +34,22 @@ async function displayData(data) {
             // topTeam.style.backgroundColor = `rgba(${topTeamColors.colors.primary.decimalRed}, ${topTeamColors.colors.primary.decimalGreen}, ${topTeamColors.colors.primary.decimalBlue}, var(--teamBackgroundOpacity))`;
             // botTeam.style.backgroundColor = `rgba(${botTeamColors.colors.primary.decimalRed}, ${botTeamColors.colors.primary.decimalGreen}, ${botTeamColors.colors.primary.decimalBlue}, var(--teamBackgroundOpacity))`;
 
-            topTeam.getElementsByClassName("logo")[0].src = `https://assets.nhle.com/logos/nhl/svg/${data.series[i].topTeam.abbr}_dark.svg`;
-            topTeam.getElementsByClassName("wins")[0].innerHTML = data.series[i].topTeam.wins;
-            botTeam.getElementsByClassName("logo")[0].src = `https://assets.nhle.com/logos/nhl/svg/${data.series[i].botTeam.abbr}_dark.svg`;
-            botTeam.getElementsByClassName("wins")[0].innerHTML = data.series[i].botTeam.wins;
+            if (data.series[i].topTeam.id != 0) {
+                topTeam.getElementsByClassName("logo")[0].src = `https://assets.nhle.com/logos/nhl/svg/${data.series[i].topTeam.abbr}_dark.svg`;
+                topTeam.getElementsByClassName("wins")[0].innerHTML = data.series[i].topTeam.wins;
+            } else {
+                topTeam.getElementsByClassName("logo")[0].style.display = "none";
+                topTeam.getElementsByClassName("wins")[0].style.display = "none";
+            }
+
+            if (data.series[i].botTeam.id != 0) {
+                botTeam.getElementsByClassName("logo")[0].src = `https://assets.nhle.com/logos/nhl/svg/${data.series[i].botTeam.abbr}_dark.svg`;
+                botTeam.getElementsByClassName("wins")[0].innerHTML = data.series[i].botTeam.wins;
+            } else {
+                botTeam.getElementsByClassName("logo")[0].style.display = "none";
+                botTeam.getElementsByClassName("wins")[0].style.display = "none";
+            }
+
 
             if (data.series[i].topTeam.wins == 4) {
                 botTeam.classList.add("seriesLoser");
