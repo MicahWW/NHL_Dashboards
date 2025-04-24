54365468d45f967253382ccb600b40251698ad57
Micah_Warren <26724012+MicahWW@users.noreply.github.com>
2025-04-24 11:40:56 -0500

    Standings date validation to be in season

--- Services/NhlApi.cs
+++ Services/NhlApi.cs
@@ -33,6 +33,30 @@ public static class NhlApi
     }
 
     /// <summary>
+    /// Validates if the date passed in is within the range of any of the seasons in the standings-season endpoint.
+    /// </summary>
+    /// <param name="httpClient">A httpClient setup with the NHL API base URL.</param>
+    /// <param name="date">A date to be checked if within a season.</param>
+    /// <returns>Returns true if the date is within a season and false if otherwise.</returns>
+    private static async Task<bool> IsValidStandingSeasonDateAsync(HttpClient httpClient, string date)
+    {
+        var StandingsSeason = await GetStandingsSeasonAsync(httpClient);
+
+        foreach (var season in StandingsSeason.Seasons)
+        {
+            var start = DateTime.ParseExact(season.StandingsStart, "yyyy-MM-dd", null);
+            var end = DateTime.ParseExact(season.StandingsEnd, "yyyy-MM-dd", null);
+            var userDate = DateTime.ParseExact(date, "yyyy-MM-dd", null);
+
+            if (userDate >= start && userDate <= end)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /// <summary>
     /// Grabs data from the NHL standings endpoint. Specifying a date will return the standings for that date, if no
     /// date is specified it will return the standings for the latest date possible.
     /// </summary>
@@ -51,6 +75,9 @@ public static class NhlApi
             // Validate the date format to be yyyy-MM-dd
             if (!DateTime.TryParseExact(date, "yyyy-MM-dd", null, System.Globalization.DateTimeStyles.None, out _))
                 throw new Exception("The date passed in was not in the correct format. Please use yyyy-MM-dd.");
+
+            if (!await IsValidStandingSeasonDateAsync(httpClient, date))
+                throw new Exception("The date given was not in the range of any season. Please use a date between the start and end of a season.");
         }
 
         using var response = await httpClient.GetAsync($"v1/standings/{date}");
